e990aba4fbdeaba28c3a83de8624bdbdd26450ca
jms <minsoub@bithumbsystems.com>
2022-06-18 22:53:50 +0900

    AUTH URL 변경

--- src/apis/axiosAuth.js
+++ src/apis/axiosAuth.js
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstanceAuth = axios.create({
-    baseURL: process.env.REACT_APP_DEFAULTAUTH_URL, // 기본 서버 주소 입력
+    baseURL: process.env.REACT_APP_DEFAULT_AUTH_URL, // 기본 서버 주소 입력
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json;charset=UTF-8'
