012d3e7ebb71dba5df9af8dc2839b8c93bcd81f7
hgjeong <hgjeong@bithumbsystems.com>
2022-07-28 22:02:17 +0900

    로그 제거

--- src/utils/CommonUtils.js
+++ src/utils/CommonUtils.js
@@ -5,7 +5,6 @@ export const  getDateFormat = (paramDate) => {
     if(paramDate){
         const dateFormat = 'YYYY-MM-DD HH:mm';
         const newDate = moment(paramDate).add(9, 'h').format(dateFormat);
-        console.log('getDateFormat:', paramDate, newDate);
         return newDate;
     }
     return '';
