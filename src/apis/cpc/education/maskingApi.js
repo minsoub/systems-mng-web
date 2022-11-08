a0472fd5444dc7576ab5083325fd82d896185b65
jhjeong <51750678+khunjeong@users.noreply.github.com>
2022-11-08 12:14:40 +0900

    Refactor: education api 병합
    education api 병합

--- src/apis/cpc/education/maskingApi.js
+++ src/apis/cpc/education/maskingApi.js
@@ -8,13 +8,43 @@ const MaskingApis = () => {
     const getMaskings = (request) => {
         const start_date = request.start_date;
         const end_date = request.end_date;
-        const is_answer_complete = request.category == undefined ? '' : request.category;
-        const keyword = request.keyword == undefined ? '' : encodeURIComponent(request.keyword);
+        const is_answer_complete = encodeURIComponent(request.category);
+        const keyword = request.keyword.length === 0 ? undefined : encodeURIComponent(request.keyword);
         callApi('getMaskings', {
             axiosInstance: axiosInstanceDefault,
             method: 'get',
-            url: `/mng/cpc/education/masking?start_date=${start_date}&end_date=${end_date}&is_answer_complete=${is_answer_complete}&keyword=${keyword}`,
-            requestConfig: {}
+            url: `/mng/cpc/education/masking`,
+            requestConfig: {
+                params: {
+                    start_date,
+                    end_date,
+                    is_answer_complete,
+                    keyword
+                }
+            }
+        });
+    };
+
+    // 신청자 관리 목록 조회 - 마스킹 해제
+    const getUnMaskings = (request) => {
+        const start_date = request.start_date;
+        const end_date = request.end_date;
+        const is_answer_complete = encodeURIComponent(request.category);
+        const keyword = request.keyword.length === 0 ? undefined : encodeURIComponent(request.keyword);
+        const reason = request.reason;
+        callApi('getUnMaskings', {
+            axiosInstance: axiosInstanceDefault,
+            method: 'get',
+            url: `/mng/cpc/education/unmasking`,
+            requestConfig: {
+                params: {
+                    start_date,
+                    end_date,
+                    is_answer_complete,
+                    keyword,
+                    reason
+                }
+            }
         });
     };
 
@@ -29,13 +59,31 @@ const MaskingApis = () => {
         });
     };
 
+    // 신청자 관리 상세 조회 - 마스킹 해제
+    const getEducationUnMasking = (request) => {
+        const id = request.id;
+        const reason = request.reason;
+        callApi('getEducationMasking', {
+            axiosInstance: axiosInstanceDefault,
+            method: 'get',
+            url: `/mng/cpc/education/${id}/masking`,
+            requestConfig: {
+                params: {
+                    reason
+                }
+            }
+        });
+    };
+
     return [
         responseData,
         requestError,
         loading,
         {
             searchEducationList: getMaskings,
-            searchEducation: getEducationMasking
+            searchUnMaskingList: getUnMaskings,
+            searchEducation: getEducationMasking,
+            searchUnMaskingEducation: getEducationUnMasking
         }
     ];
 };
