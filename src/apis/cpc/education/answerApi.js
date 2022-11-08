88fbc6dab878d59a62c8e8836d598eccecdb17b2
jhjeong <51750678+khunjeong@users.noreply.github.com>
2022-11-08 12:09:38 +0900

    FEAT: 신청자 관리 답변 저장 API 추가
    (put) /api/v1/mng/cpc/education/{id}

---
+++ src/apis/cpc/education/answerApi.js
@@ -0,0 +1,37 @@
+import axiosInstanceDefault from '../../axiosDefault';
+import useAxios from '../../useAxios';
+
+const AnswerApi = () => {
+    const [responseData, requestError, loading, callApi] = useAxios();
+
+    // 신청자 관리 답변 저장
+    const putAnswer = (request) => {
+        const id = request.id;
+        const isEmail = request.isEmail;
+        const answer = request.answer;
+        const isMasking = request.isMasking;
+        callApi('putAnswer', {
+            axiosInstance: axiosInstanceDefault,
+            method: 'put',
+            url: `/api/v1/mng/cpc/education/${id}`,
+            requestConfig: {},
+            data: {
+                id,
+                isEmail,
+                answer,
+                isMasking
+            }
+        });
+    };
+
+    return [
+        responseData,
+        requestError,
+        loading,
+        {
+            sendAnswer: putAnswer
+        }
+    ];
+};
+
+export default AnswerApi;
