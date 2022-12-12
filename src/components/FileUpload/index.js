993a6e161ebdbf18ce5218c6840ae74a04349095
jhjeong <51750678+khunjeong@users.noreply.github.com>
2022-12-12 15:59:31 +0900

    Refactor: FileUpload component 추가
    - DND, multi 업로드
    - api 연동 x
    
    Fixes: @LIST-215

--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -1,31 +1,45 @@
-import { UploadOutlined } from '@ant-design/icons';
+import React, { useCallback, useEffect } from 'react';
 import { Button, message, Upload } from 'antd';
-import React from 'react';
-const props = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    headers: {
-        authorization: 'authorization-text'
-    },
+import { UploadOutlined } from '@ant-design/icons';
+import { useDropzone } from 'react-dropzone';
+import { humanFileSize } from 'utils/CommonUtils';
+import './styles.scss';
 
-    onChange(info) {
-        if (info.file.status !== 'uploading') {
-            console.log(info.file, info.fileList);
+const FileUpload = (id, insertChatFile) => {
+    const onDrop = useCallback((acceptedFiles) => {
+        if (!file) {
+            alert('파일을 업로드 하지 않았습니다.');
+            return;
         }
+        Promise.all(
+            acceptedFiles.map((file) => {
+                const formData = new FormData();
+                formData.append('file', file);
+                formData.append('projectId', id);
+                formData.append('fileName', file.name);
+                formData.append('fileType', file.type);
+                // 3.2MB로 계산하기
+                formData.append('fileSize', humanFileSize(file.size, true, 2));
 
-        if (info.file.status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-    onClick: () => {}
+                return insertChatFile(formData);
+            })
+        )
+            .then((response) => {
+                console.log({ response });
+            })
+            .catch((error) => {
+                console.log({ error });
+            });
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+    return (
+        <div {...getRootProps({ className: 'dropzone' })}>
+            <input {...getInputProps()} />
+            <p className="txt">마우스로 파일을 끌어오세요.</p>
+        </div>
+    );
 };
 
-const App = () => (
-    <Upload {...props}>
-        <Button icon={<UploadOutlined />}>업로드</Button>
-    </Upload>
-);
-
-export default App;
+export default FileUpload;
