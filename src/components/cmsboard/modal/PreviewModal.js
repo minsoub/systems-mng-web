fbaf4c1668a459c511273ec4e5c2ba792d1244ad
fe_user_01-at-807380035085 <junhoh0102@bithumbsysttems.com>
2023-01-11 16:17:53 +0900

    콘텐츠 통합 미리보기 팝업

--- src/components/cmsboard/modal/PreviewModal.js
+++ src/components/cmsboard/modal/PreviewModal.js
@@ -7,7 +7,6 @@ import { Box, Modal } from '@mui/material';
 //library
 import PropTypes from 'prop-types';
 
-
 // style
 import styles from './styles.module.scss';
 
@@ -16,10 +15,17 @@ import styles from './styles.module.scss';
 const PreviewModal = ({ open, onClose, viewMode }) => {
     const { reduceTitle } = useSelector((state) => state.cmsDetailData);
     const [editContents, setEditContents] = useState('');
+    const [viewModeStyle, setViewModeStyle] = useState(styles.false);
 
     useEffect(() => {
         if (!viewMode) return;
         console.log(viewMode);
+        if (viewMode === 'pc') {
+            setViewModeStyle(styles.pc);
+        } else {
+            setViewModeStyle(styles.mobile);
+        }
+        console.log(viewModeStyle);
     }, [viewMode]);
 
     useEffect(() => {
@@ -30,10 +36,19 @@ const PreviewModal = ({ open, onClose, viewMode }) => {
 
     return (
         <Modal open={open} onClose={onClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-            <Box className={`${styles.preview_modal_wrap} ${viewMode}`}>
-                <h2 className={`${styles.modal_title}`}>{reduceTitle}</h2>
-                <div>2023-01-01 00:00:00</div>
-                <div className="common-board--layout" dangerouslySetInnerHTML={{ __html: editContents }}></div>
+            <Box className={`${styles.preview_modal_wrap} ${viewModeStyle}`}>
+                <div className={styles.preview_modal_box}>
+                    <div className={styles.modal_header}>
+                        <h2 className={`${styles.modal_header__title}`}>{reduceTitle}</h2>
+                        <p className={styles.modal_header_box}>
+                            <span className={styles.modal_header__date}>2023-01-01 00:00:00</span>
+                        </p>
+                    </div>
+                    <div
+                        className={`common-board--layout ${styles.modal_content}`}
+                        dangerouslySetInnerHTML={{ __html: editContents }}
+                    ></div>
+                </div>
             </Box>
         </Modal>
     );
